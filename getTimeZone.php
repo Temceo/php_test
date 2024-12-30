d2abb13d3223cb061f4066b8fa6532515bb5101d
Martin Mwaka <martinmwaka@hotmail.com>
2024-12-30 10:11:49 +0000

    Update getTimeZone.php

--- getTimeZone.php
+++ getTimeZone.php
@@ -31,9 +31,7 @@ $output["status"]["code"] = "200";
 $output["status"]["name"] = "ok";
 $output["status"]["description"] = "success";
 $output["status"]["returnedIn"] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
-$output["data"] = $decode["timezone"];
 
-header("Content-Type: application/json; charset=UTF-8");
 
 echo json_encode($output);
 echo json_encode($result);
